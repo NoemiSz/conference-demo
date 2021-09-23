84c98e973ff1bf15b3b19d9e8f3827a8f644edcb
NoemiSz <szokenoemijob@gmail.com>
2021-09-23 14:13:31 +0200

    For home use

---
+++ src/main/java/com/example/conferencedemo/controllers/SpeakersController.java
@@ -0,0 +1,46 @@
+package com.example.conferencedemo.controllers;
+
+import com.example.conferencedemo.models.Speaker;
+import com.example.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list(){
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @DeleteMapping(value = "{id}")
+    public void delete(@PathVariable  Long id){
+        speakerRepository.deleteById(id);
+    }
+
+    @PutMapping(value = "{id}")
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        Speaker previousSpeaker = speakerRepository.getById(id);
+        BeanUtils.copyProperties(speaker, previousSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(previousSpeaker);
+    }
+
+}
